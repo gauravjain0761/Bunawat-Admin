36f9bbc9d55fe855e711a57309c8d8ac0d0805f7
arshit <frontend280@gmail.com>
2023-07-04 14:44:56 +0530

    some changes

--- src/app/utils/utils.js
+++ src/app/utils/utils.js
@@ -1,14 +1,14 @@
 
 export const convertHexToRGB = (hex) => {
   // check if it's a rgba
-  if (hex?.match('rgba')) {
+  if (hex.match('rgba')) {
     let triplet = hex.slice(5).split(',').slice(0, -1).join(',');
     return triplet;
   }
 
   let c;
   if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
-    c = hex?.substring(1).split('');
+    c = hex.substring(1).split('');
     if (c.length === 3) {
       c = [c[0], c[0], c[1], c[1], c[2], c[2]];
     }
